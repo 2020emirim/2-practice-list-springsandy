325f081024c791bc1af41aa1f0a479e06511eb6f
Kim Bom-i <s2018w26@e-mirim.hs.kr>
2020-05-22 15:31:48 +0900

    models get_absolute_url() detail

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Bookmark(models.Model):
@@ -6,4 +7,7 @@ class Bookmark(models.Model):
     url = models.URLField('Site URL')
 
     def __str__(self):
-        return "제목 :"+self.site_name+", URL :"+self.url
+        return "제목 :" + self.site_name + ", URL :" + self.url
+
+    def get_absolute_url(self):
+        return reverse('bookmark:detail', kwargs={'pk': self.id})
